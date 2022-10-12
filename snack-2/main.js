c14531c5ad105e6e7dd7c012a5b31afeca25df90
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-10-12 12:15:47 +0200

    complete

--- snack-2/main.js
+++ snack-2/main.js
@@ -10,12 +10,18 @@ if (p1.length > p2.length){
     console.log(p1);
     console.log('La prima parola è più lunga della seconda');
     console.log(p2);
+    document.getElementById('parole').innerHTML ='La prima parola è più lunga della seconda' + p1 ;
+
 } else if (p1.length < p2.length){
     console.log(p2);
     console.log('La seconda parola è più lunga della prima');
     console.log(p1);
+    document.getElementById('parole').innerHTML ='La seconda parola è più lunga della prima' + p2 ;
+
 } else{
     console.log(' Le due parole hanno la stessa lunghezza');
     console.log(p1);
     console.log(p2);
+    document.getElementById('parole').innerHTML ='Le due parole hanno la stessa lunghezza' + p1 + p2 ;
+
 }
